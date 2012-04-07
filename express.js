cd705f064cf18010cfc6f4dfd736f649eb28fc7b
Iván Fernández <ivan.fernandez.perea@gmail.com>
2012-04-07 06:24:18 +0200

    Removed some files

---
+++ express.js
@@ -0,0 +1,37 @@
+var express = require('express');
+
+var app = express.createServer();
+
+app.configure(function() {
+	app.use(express.logger());
+	app.use(express.static(__dirname + '/static'));
+})
+
+app.configure('development', function() {
+	app.use(express.errorHandler({
+		dumpExceptions: true,
+		showStack: true
+	}));
+});
+
+app.configure('production', function() {
+	app.use(express.errorHandler());
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+//app.set('view options', {layout: false});
+
+//app.set('view options', {layout: true, locals: { scripts: ['no.js'] }});
+
+app.get('/', function(req,res){
+	/*res.render(__dirname + '/views/' + 'root', {
+		scripts: ['no.js']
+	});*/
+	res.render('root');
+});
+
+app.listen(4000);
+
+
+
